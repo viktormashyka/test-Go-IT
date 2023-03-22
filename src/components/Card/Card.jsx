ffb1df512255e68f0ec00b80ba4ccadca223a719
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2023-03-22 21:48:59 +0000

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,26 +3,23 @@ import { Wrapper, Text1, Text2, Button, Title } from './Card.styled';
 
 export const Card = () => {
   const [tweets] = useState(777);
-  const [followers, setFollowers] = useState(100500);
-  const [isChecked, setIsChecked] = useState(false);
+  const [followers, setFollowers] = useState(() => {
+    return JSON.parse(localStorage.getItem('myFollowers')) ?? 100500;
+  });
+  const [isChecked, setIsChecked] = useState(() => {
+    return JSON.parse(localStorage.getItem('myIsChecked')) ?? false;
+  });
 
   useEffect(() => {
-    const savedFollowers = localStorage.getItem('myFollowers');
-    const savedIsChecked = localStorage.getItem('myIsChecked');
-
-    if (savedFollowers) {
-      setFollowers(savedFollowers);
-    }
-    if (savedIsChecked) {
-      setIsChecked(savedIsChecked);
-    }
-  }, []);
+    localStorage.setItem('myFollowers', JSON.stringify(followers));
+    localStorage.setItem('myIsChecked', JSON.stringify(isChecked));
+  }, [followers, isChecked]);
 
   const onFollow = () => {
-    isChecked ? setFollowers(prev => prev - 1) : setFollowers(prev => prev + 1);
     setIsChecked(prev => !prev);
-    localStorage.setItem('myFollowers', followers + (isChecked ? -1 : 1));
-    localStorage.setItem('myIsChecked', !isChecked);
+    !isChecked
+      ? setFollowers(prev => prev + 1)
+      : setFollowers(prev => prev - 1);
   };
 
   function commafy(num) {
@@ -37,7 +34,7 @@ export const Card = () => {
       <Text2>{followersNormalize} Followers</Text2>
       <Button
         type="button"
-        onClick={() => onFollow()}
+        onClick={onFollow}
         style={{
           background: !isChecked ? '#ebd8ff' : '#5cd3a8',
         }}
